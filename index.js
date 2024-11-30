8c342d6b88ea303b731c8aa16d4f457ba6db7ff3
Billy Eskein <billyeskein@gmail.com>
2024-11-30 05:35:55 +0300

    Modify index.js

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+console.log("Multiplication:", multiply);
+//=>62
 
-// Write your code here
+let random = Math.floor(Math.random() * 100) + 1;
+console.log("Random number:", random);
+
+let num3 = 32; 
+let num4 = 7;
+let mod = (32 % 7);
+//=> 4
+
+let numbers = [5,15,20,10,8];
+let max = 20;
+Math.max(5,15,20,10,8);
+// => 20
+
